78422b32296c75fc3b8d422409e26a2252f6299a
vasudevcb <137030798+vasudevcb@users.noreply.github.com>
2023-07-28 10:42:45 +0530

    Add files via upload

--- Python/Activities/conftest.py
+++ Python/Activities/conftest.py
@@ -1,19 +1,35 @@
+"""
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
 # Create a fixture to initialize our webdriver
 @pytest.fixture(scope="session")
-def setup1(request):
+def setup(request):
     # Initialize the webdriver
     driver = webdriver.Firefox()
     # Get the underlying collection
     session = request.node
     # Pass through the driver to the test class
-    for item in session.items:
+        for item in session.items:
         cls = item.getparent(pytest.Class)
-    setattr(cls.obj, "driver", driver)
-# Execute the test method
+        setattr(cls.obj, "driver", driver)
+    # Execute the test method
     yield
-# Close the browser once the test has ended
+    # Close the browser once the test has ended
     request.addfinalizer(driver.close)
+"""
+
+import pytest
+
+# Create fixture
+@pytest.fixture
+def num_list():
+
+    # Create list
+    list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    # Alternatively
+    # list = list(range(11))
+
+    return list
